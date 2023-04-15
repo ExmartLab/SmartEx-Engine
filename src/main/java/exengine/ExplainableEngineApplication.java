f7b939cc1c4cd7a91abde5fba5d9654b89caf297
Max Unterbusch <max@MBP-von-Max.fritz.box>
2023-04-15 13:53:22 +0200

    refactor: omitted scenarioId, merged demoEntry

--- src/main/java/exengine/ExplainableEngineApplication.java
+++ src/main/java/exengine/ExplainableEngineApplication.java
@@ -23,7 +23,6 @@ public class ExplainableEngineApplication implements CommandLineRunner {
 
 	public static boolean debug = true;
 	public static boolean testing = true;
-	public static int testingScenario = 1;
 
 	public static ArrayList<LogEntry> demoEntries;
 
@@ -47,20 +46,20 @@ public class ExplainableEngineApplication implements CommandLineRunner {
 
 	private void initializeTestEntityRepository() {
 		dataSer.deleteAllEntities();
-		
+
 		Entity newEntity = new Entity("sensor.lab_fan_current_consumption", "Lab Fan");
 		dataSer.saveNewEntity(newEntity);
-		
+
 		newEntity = new Entity("switch.lab_fan", "Lab Fan");
 		dataSer.saveNewEntity(newEntity);
-		
+
 		newEntity = new Entity("sensor.door_power", "Lab Door");
 		dataSer.saveNewEntity(newEntity);
-		
+
 		ArrayList<Entity> entities = dataSer.findEntitiesByDeviceName("Lab Fan");
 		System.out.println("first:" + entities.get(0).getEntityId());
 		System.out.println("second:" + entities.get(1).getEntityId());
-		
+
 	}
 
 	// initializes a Repository with Users for demonstration and testing in the
@@ -103,21 +102,21 @@ public class ExplainableEngineApplication implements CommandLineRunner {
 		ArrayList<String> conditions;
 		ArrayList<LogEntry> actions;
 
-		initiateDemoEntries(1);
+		initiateDemoEntries();
 		triggers = new ArrayList<LogEntry>();
-		triggers.add(demoEntries.get(0));
+		triggers.add(demoEntries.get(3));
 		actions = new ArrayList<LogEntry>();
-		actions.add(demoEntries.get(2));
+		actions.add(demoEntries.get(5));
 		conditions = new ArrayList<String>();
 		conditions.add("daily energy consumption is higher than the threshold");
 		dataSer.saveNewRule(new Rule("rule 1 (coffee)", "1", demoEntries.get(1), triggers, conditions, actions, idAlice,
 				"Rule_1: allows coffee to be made only until the daily energy consumption threshold is reached"));
 
-		initiateDemoEntries(2);
+		// initiateDemoEntries(2);
 		triggers = new ArrayList<LogEntry>();
-		triggers.add(demoEntries.get(1));
+		triggers.add(demoEntries.get(7));
 		actions = new ArrayList<LogEntry>();
-		actions.add(demoEntries.get(3));
+		actions.add(demoEntries.get(9));
 		conditions = new ArrayList<String>();
 		conditions.add("a meeting in room 1 is going on");
 //		actions.add("tv_mute null");
@@ -125,11 +124,12 @@ public class ExplainableEngineApplication implements CommandLineRunner {
 		dataSer.saveNewRule(new Rule("rule 2 (tv mute)", "2", demoEntries.get(2), triggers, conditions, actions, idBob,
 				"Rule_2: mutes the TV if TV is playing while a meeting is going on"));
 
-		initiateDemoEntries(5);
+		// initiateDemoEntries(5);
 		actions = new ArrayList<LogEntry>();
 		actions.add(demoEntries.get(2));
-		dataSer.saveNewError(new Error("Deebot error", "e1", actions, "the robotic vacuum cleaner is stuck", "remove barrier or set robot back on track"));
-				
+		dataSer.saveNewError(new Error("Deebot error", "e1", actions, "the robotic vacuum cleaner is stuck",
+				"remove barrier or set robot back on track"));
+
 	}
 
 	public void initializeTestOccurrenceRepository() {
@@ -141,96 +141,108 @@ public class ExplainableEngineApplication implements CommandLineRunner {
 	}
 
 	// initiates the demoEntries-List with LogEntries for Demonstration and Testing
-	public static void initiateDemoEntries(int scenario) {
+	public static void initiateDemoEntries() {
 		demoEntries = new ArrayList<LogEntry>();
 		ArrayList<String> other;
-		switch (scenario) {
-		case 1:
 
-			// trigger LogEntry
-			demoEntries.add(
-					new LogEntry("2022-06-23T09:50:50.014573+00:00", "state change", null, "scene.state_change", null));
+		// TODO name LogEntries in error case
+		demoEntries.add(new LogEntry("2022-06-23T09:07:26.920189+00:00", "Deebot", "idle", "vacuum.deebot", null)); // 0
+																													// scenario
+																													// 5:
+																													// index:
+																													// 0
+		demoEntries.add(new LogEntry("2022-06-23T09:07:26.932243+00:00", "Deebot", "error", "vacuum.deebot", null)); // 1
+																														// scenario
+																														// 5:
+																														// index:
+																														// 1
 
-			// rule LogEntry
-			other = new ArrayList<String>();
-			other.add("message\": \"triggered by state of sensor.smart_plug_social_room_coffee_today_s_consumption");
-			other.add("source\": \"state of sensor.smart_plug_social_room_coffee_today_s_consumption");
-			other.add("context_id\": \"01G67ZHDBKS302M9XP2GJTZAJH\", \"domain\": \"automation");
-			demoEntries.add(new LogEntry("2022-06-23T09:50:50.229746+00:00", "sc1: Goal-Order-Conflict", null,
-					"automation.test_scenario_watching_tv_light_off", other));
+		other = new ArrayList<String>();
+		other.add("icon\": \"mdi:alert-circle");
+		demoEntries.add(new LogEntry("2022-06-23T09:07:26.933444+00:00", "Deebot last error", "104", // 2 scenario 5:
+																										// index: 2
+				"sensor.deebot_last_error", other));
 
-			// action LogEntry
-			other = new ArrayList<String>();
-			other.add("context_event_type\": \"automation_triggered");
-			other.add("context_domain\": \"automation");
-			other.add("context_name\": \"sc1: Goal-Order-Conflict");
-			other.add(
-					"context_message\": \"triggered by state of sensor.smart_plug_social_room_coffee_today_s_consumption");
-			other.add("context_source\": \"state of sensor.smart_plug_social_room_coffee_today_s_consumption");
-			other.add("context_entity_id\": \"automation.test_scenario_watching_tv_light_off");
-			other.add("context_entity_id_name\": \"sc1: Goal-Order-Conflict");
-			demoEntries.add(new LogEntry("2022-06-23T09:50:50.848452+00:00", "Smart Plug Social Room Coffee", "off",
-					"switch.smart_plug_social_room_coffee", other));
-			break;
+		// trigger LogEntry
+		demoEntries.add(
+				new LogEntry("2022-06-23T09:50:50.014573+00:00", "state change", null, "scene.state_change", null)); // 3
+																														// scenario
+																														// 1:
+																														// index:
+																														// 0
 
-		case 2:
-			other = new ArrayList<String>();
-			other.add("context_event_type\": \"automation_triggered");
-			other.add("context_domain\": \"automation");
-			other.add("context_name\": \"Welcome");
-			other.add("context_message\": \"triggered by state of binary_sensor.door");
-			other.add("context_source\": \"state of binary_sensor.door");
-			other.add("context_entity_id\": \"automation.presence_notification");
-			other.add("context_entity_id_name\": \"Welcome");
-			demoEntries.add(
-					new LogEntry("2022-06-23T11:19:30.181206+00:00", "Lab TV", "idle", "media_player.lab_tv", other));
+		// rule LogEntry
+		other = new ArrayList<String>();
+		other.add("message\": \"triggered by state of sensor.smart_plug_social_room_coffee_today_s_consumption");
+		other.add("source\": \"state of sensor.smart_plug_social_room_coffee_today_s_consumption");
+		other.add("context_id\": \"01G67ZHDBKS302M9XP2GJTZAJH\", \"domain\": \"automation");
+		demoEntries.add(new LogEntry("2022-06-23T09:50:50.229746+00:00", "sc1: Goal-Order-Conflict", null,
+				"automation.test_scenario_watching_tv_light_off", other)); // 4 scenario 1: index: 1
 
-			// trigger LogEntry
-			demoEntries.add(
-					new LogEntry("2022-06-23T11:19:31.024951+00:00", "Lab TV", "playing", "media_player.lab_tv", null));
+		// action LogEntry
+		other = new ArrayList<String>();
+		other.add("context_event_type\": \"automation_triggered");
+		other.add("context_domain\": \"automation");
+		other.add("context_name\": \"sc1: Goal-Order-Conflict");
+		other.add(
+				"context_message\": \"triggered by state of sensor.smart_plug_social_room_coffee_today_s_consumption");
+		other.add("context_source\": \"state of sensor.smart_plug_social_room_coffee_today_s_consumption");
+		other.add("context_entity_id\": \"automation.test_scenario_watching_tv_light_off");
+		other.add("context_entity_id_name\": \"sc1: Goal-Order-Conflict");
+		demoEntries.add(new LogEntry("2022-06-23T09:50:50.848452+00:00", "Smart Plug Social Room Coffee", "off",
+				"switch.smart_plug_social_room_coffee", other)); // 5 scenario 1: index: 2
 
-			// rule LogEntry
-			other = new ArrayList<String>();
-			other.add("message\": \"triggered by state of media_player.lab_tv");
-			other.add("source\": \"state of media_player.lab_tv");
-			other.add("context_id\": \"01G684KSEJHD3DRWH9K36578E9");
-			other.add("domain\": \"automation");
-			other.add("context_state\": \"playing");
-			other.add("context_entity_id\": \"media_player.lab_tv");
-			other.add("context_entity_id_name\": \"Lab TV");
-			demoEntries.add(new LogEntry("2022-06-23T11:19:31.028089+00:00", "sc2: Multi-User-Conflict", "null",
-					"automation.sc2_multi_user_conflict", other));
+		other = new ArrayList<String>();
+		other.add("context_event_type\": \"automation_triggered");
+		other.add("context_domain\": \"automation");
+		other.add("context_name\": \"Welcome");
+		other.add("context_message\": \"triggered by state of binary_sensor.door");
+		other.add("context_source\": \"state of binary_sensor.door");
+		other.add("context_entity_id\": \"automation.presence_notification");
+		other.add("context_entity_id_name\": \"Welcome");
+		demoEntries
+				.add(new LogEntry("2022-06-23T11:19:30.181206+00:00", "Lab TV", "idle", "media_player.lab_tv", other)); // 6
+																														// scenario
+																														// 2:
+																														// index:
+																														// 0
 
-			// action LogEntry
-			other = new ArrayList<String>();
-			other.add("icon\": \"mdi:television");
-			other.add("context_event_type\": \"automation_triggered");
-			other.add("context_domain\": \"automation");
-			other.add("context_name\": \"sc2: Multi-User-Conflict");
-			other.add("context_message\": \"triggered by state of media_player.lab_tv");
-			other.add("context_source\": \"state of media_player.lab_tv");
-			other.add("context_entity_id\": \"automation.sc2_multi_user_conflict");
-			other.add("context_entity_id_name\": \"sc2: Multi-User-Conflict");
-			demoEntries
-					.add(new LogEntry("2022-06-23T11:19:31.037231+00:00", "tv_mute", null, "scene.tv_playing", other));
-			break;
+		// trigger LogEntry
+		demoEntries.add(
+				new LogEntry("2022-06-23T11:19:31.024951+00:00", "Lab TV", "playing", "media_player.lab_tv", null)); // 7
+																														// scenario
+																														// 2:
+																														// index:
+																														// 1
 
-		case 4:
+		// rule LogEntry
+		other = new ArrayList<String>();
+		other.add("message\": \"triggered by state of media_player.lab_tv");
+		other.add("source\": \"state of media_player.lab_tv");
+		other.add("context_id\": \"01G684KSEJHD3DRWH9K36578E9");
+		other.add("domain\": \"automation");
+		other.add("context_state\": \"playing");
+		other.add("context_entity_id\": \"media_player.lab_tv");
+		other.add("context_entity_id_name\": \"Lab TV");
+		demoEntries.add(new LogEntry("2022-06-23T11:19:31.028089+00:00", "sc2: Multi-User-Conflict", "null",
+				"automation.sc2_multi_user_conflict", other)); // 8 scenario 2: index: 2
 
-			break;
+		// action LogEntry
+		other = new ArrayList<String>();
+		other.add("icon\": \"mdi:television");
+		other.add("context_event_type\": \"automation_triggered");
+		other.add("context_domain\": \"automation");
+		other.add("context_name\": \"sc2: Multi-User-Conflict");
+		other.add("context_message\": \"triggered by state of media_player.lab_tv");
+		other.add("context_source\": \"state of media_player.lab_tv");
+		other.add("context_entity_id\": \"automation.sc2_multi_user_conflict");
+		other.add("context_entity_id_name\": \"sc2: Multi-User-Conflict");
+		demoEntries.add(new LogEntry("2022-06-23T11:19:31.037231+00:00", "tv_mute", null, "scene.tv_playing", other)); // 9
+																														// scenario
+																														// 2:
+																														// index:
+																														// 3
 
-		case 5:
-			// TODO name LogEntries in error case
-			demoEntries.add(new LogEntry("2022-06-23T09:07:26.920189+00:00", "Deebot", "idle", "vacuum.deebot", null));
-			demoEntries.add(new LogEntry("2022-06-23T09:07:26.932243+00:00", "Deebot", "error", "vacuum.deebot", null));
-
-			other = new ArrayList<String>();
-			other.add("icon\": \"mdi:alert-circle");
-			demoEntries.add(new LogEntry("2022-06-23T09:07:26.933444+00:00", "Deebot last error", "104",
-					"sensor.deebot_last_error", other));
-
-			break;
-		}
 	}
 
 }
