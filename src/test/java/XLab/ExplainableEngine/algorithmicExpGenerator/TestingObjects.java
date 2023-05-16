bc343c808de2a3db3cd2b70110e93bec34ecf895
Max Unterbusch <max@MBP-von-Max.fritz.box>
2023-05-16 17:11:31 +0200

    add: unit test for causal explanation generation based on test cases from the paper

---
+++ src/test/java/XLab/ExplainableEngine/algorithmicExpGenerator/TestingObjects.java
@@ -0,0 +1,89 @@
+package XLab.ExplainableEngine.algorithmicExpGenerator;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import exengine.ExplainableEngineApplication;
+import exengine.datamodel.Error;
+import exengine.datamodel.LogEntry;
+import exengine.datamodel.Rule;
+import exengine.loader.JsonHandler;
+
+/*
+ * Loads Objects containing testing data and testing data only.
+ * 
+ * This class shall be used to test the explanation generation process. 
+ * 
+ * For this purpose, we do not use the actual database for testing since we want the unit tests 
+ * to be uncoupled from the database. Thereby, testing with this data does not require to have 
+ * the database always in a state that contains the test data.
+ */
+class TestingObjects {
+
+	private ArrayList<LogEntry> demoEntries;
+	private ArrayList<Rule> dbRules;
+	private ArrayList<Error> dbErrors;
+
+	TestingObjects() throws IOException, URISyntaxException {
+		// populate demoEntries
+		String logJSON = JsonHandler.loadFile(ExplainableEngineApplication.FILE_NAME_DEMO_LOGS);
+		demoEntries = JsonHandler.loadLogEntriesFromJson(logJSON);
+		
+		populateRules();
+		populateErrors();
+	}
+
+	private void populateRules() {
+		dbRules = new ArrayList<Rule>();
+
+		ArrayList<LogEntry> triggers;
+		ArrayList<String> conditions;
+		ArrayList<LogEntry> actions;
+
+		triggers = new ArrayList<LogEntry>();
+		triggers.add(demoEntries.get(3));
+		actions = new ArrayList<LogEntry>();
+		actions.add(demoEntries.get(5));
+		conditions = new ArrayList<String>();
+		conditions.add("daily energy consumption is higher than the threshold");
+		dbRules.add(new Rule("rule 1 (coffee)", "1", demoEntries.get(4), triggers, conditions, actions, "1",
+				"Rule_1: allows coffee to be made only until the daily energy consumption threshold is reached"));
+
+		triggers = new ArrayList<LogEntry>();
+		triggers.add(demoEntries.get(7));
+		actions = new ArrayList<LogEntry>();
+		actions.add(demoEntries.get(9));
+		conditions = new ArrayList<String>();
+		conditions.add("a meeting in room 1 is going on");
+
+		dbRules.add(new Rule("rule 2 (tv mute)", "2", demoEntries.get(8), triggers, conditions, actions, "2",
+				"Rule_2: mutes the TV if TV is playing while a meeting is going on"));
+	}
+
+	private void populateErrors() {
+		dbErrors = new ArrayList<Error>();
+
+		ArrayList<LogEntry> actions;
+
+		// populate errors
+		actions = new ArrayList<LogEntry>();
+		actions.add(demoEntries.get(2));
+		dbErrors.add(new Error("No. 104: DEEBOT gets stuck while working and stops", "04", actions,
+				"DEEBOT is tangled with something on the floor or might be stuck under furniture",
+				"If DEEBOT can not free itself, manually remove the obstacles and restart"));
+	}
+
+	ArrayList<LogEntry> getDemoEntries() {
+		return demoEntries;
+	}
+
+	ArrayList<Rule> getDBRules() {
+		return dbRules;
+	}
+
+	ArrayList<Error> getDBErrors() {
+		return dbErrors;
+	}
+
+}
